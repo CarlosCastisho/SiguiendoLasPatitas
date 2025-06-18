40edb41ef2b5e24a91ff826e0e5bbed40a255a54
Carlos Castillo <carlos.castisho@gmail.com>
2025-06-18 19:41:42 -0300

    Actualiza el script de inicio en package.json y elimina credenciales de base de datos comentadas en keys.js

--- SiguiendoLasPatitas1.0/src/keys.js
+++ SiguiendoLasPatitas1.0/src/keys.js
@@ -6,9 +6,5 @@ module.exports = {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME
-        // host: 'localhost',
-        // user: 'root',
-        // password: '94546249_albert',
-        // database: 'siguiendolaspatitas'
     }
 };
