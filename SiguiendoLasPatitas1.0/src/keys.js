3c3f089b0012ecd2952527bf1a111a8d3e2d3d80
Carlos Castillo <carlos.castisho@gmail.com>
2025-06-18 21:01:58 -0300

    Añade configuración SSL a la conexión de la base de datos y organiza el código en keys.js

--- SiguiendoLasPatitas1.0/src/keys.js
+++ SiguiendoLasPatitas1.0/src/keys.js
@@ -3,8 +3,16 @@ require('dotenv-flow').config();
 module.exports = {
     database: {
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
+        database: process.env.DB_NAME,
+        ssl: {
+            rejectUnauthorized: false
+        }
+        // host: process.env.DB_HOST,
+        // user: process.env.DB_USER,
+        // password: process.env.DB_PASSWORD,
+        // database: process.env.DB_NAME
     }
 };
