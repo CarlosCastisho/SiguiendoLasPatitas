231c2bc517b35994672d817d403b29f039749979
Carlos Castillo <carlos.castisho@gmail.com>
2025-06-20 14:33:23 -0300

    Refactor SSL configuration in keys.js to use rejectUnauthorized flag

--- SiguiendoLasPatitas1.0/src/keys.js
+++ SiguiendoLasPatitas1.0/src/keys.js
@@ -7,10 +7,9 @@ module.exports = {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        ssl: process.env.DB_SSL === 'true'
-        // host: process.env.DB_HOST,
-        // user: process.env.DB_USER,
-        // password: process.env.DB_PASSWORD,
-        // database: process.env.DB_NAME
+        ssl: {
+            rejectUnauthorized: false
+        }
+        // ssl: process.env.DB_SSL === 'true'
     }
 };
